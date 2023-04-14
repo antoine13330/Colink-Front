63f125c20684079af88e9360d6b494129f8d467d
Anthony <anthony.ribeiro69720@gmail.com>
2023-04-14 10:04:03 +0100

    profil page works

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -4,9 +4,11 @@ import { HomeComponent } from './pages/home/home.component';
 import { SigninComponent } from './_components/auth/signin/signin.component';
 import { SignupComponent } from './_components/auth/signup/signup.component';
 import { ForgotComponent } from './_components/auth/forgot/forgot.component';
+import { ProfilPageComponent } from './pages/profil-page/profil-page.component';
 
 const routes: Routes = [
   { path : '' , component : HomeComponent , pathMatch : 'full' },
+  { path : 'profil' , component : ProfilPageComponent , pathMatch : 'full' },
   { path : 'signin' , component :  SigninComponent, pathMatch : 'full' },
   { path : 'signup' , component :  SignupComponent, pathMatch : 'full' },
   { path : 'forgot' , component : ForgotComponent, pathMatch : 'full' },
