cf82dd3d06f9b342468f1d7888d544af98b035b4
Anthony <anthony.ribeiro69720@gmail.com>
2023-04-14 09:34:55 +0100

    profil component

---
+++ src/app/_components/profil-management/profil-management.component.spec.ts
@@ -0,0 +1,23 @@
+import { ComponentFixture, TestBed } from '@angular/core/testing';
+
+import { ProfilManagementComponent } from './profil-management.component';
+
+describe('ProfilManagementComponent', () => {
+  let component: ProfilManagementComponent;
+  let fixture: ComponentFixture<ProfilManagementComponent>;
+
+  beforeEach(async () => {
+    await TestBed.configureTestingModule({
+      declarations: [ ProfilManagementComponent ]
+    })
+    .compileComponents();
+
+    fixture = TestBed.createComponent(ProfilManagementComponent);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+  });
+
+  it('should create', () => {
+    expect(component).toBeTruthy();
+  });
+});
