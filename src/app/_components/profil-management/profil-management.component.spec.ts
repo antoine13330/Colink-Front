3c1f4046cb58d87f5e5a10a050775c30ecd59c07
Anthony <anthony.ribeiro69720@gmail.com>
2023-04-14 14:47:26 +0100

    edit-profig

--- src/app/_components/profil-management/profil-management.component.spec.ts
+++ src/app/_components/profil-management/profil-management.component.spec.ts
@@ -1,11 +1,7 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
-<<<<<<< HEAD
-import { ProfilManagementComponent } from './profil-management.component';
-=======
 
 import { ProfilManagementComponent } from './profil-management.component';
 
->>>>>>> d789f6c0484cb9296c1614da3fc3e558ea045ab3
 describe('ProfilManagementComponent', () => {
   let component: ProfilManagementComponent;
   let fixture: ComponentFixture<ProfilManagementComponent>;
@@ -20,10 +16,6 @@ describe('ProfilManagementComponent', () => {
     component = fixture.componentInstance;
     fixture.detectChanges();
   });
-<<<<<<< HEAD
-=======
-
->>>>>>> d789f6c0484cb9296c1614da3fc3e558ea045ab3
   it('should create', () => {
     expect(component).toBeTruthy();
   });
--- src/app/_components/profil-management/profil-management.component.ts
+++ src/app/_components/profil-management/profil-management.component.ts
@@ -7,11 +7,8 @@ import { Component } from '@angular/core';
 })
 export class ProfilManagementComponent {
 
-<<<<<<< HEAD
   isEditing:boolean=false
   onUpdateEditMode(){
     this.isEditing=!this.isEditing;
   }
-=======
->>>>>>> d789f6c0484cb9296c1614da3fc3e558ea045ab3
 }
